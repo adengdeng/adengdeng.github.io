eb61107a31286165884756fd9bea8503c68a52d7
jeff <a98208@capital.com.tw>
2024-09-10 14:03:06 +0800

    update new files

--- app_link_handler.js
+++ app_link_handler.js
@@ -1,10 +1,10 @@
-// 應用程序的 URL Scheme
+// APP的 URL Scheme
 const APP_SCHEME = "ihappytrade://";
 // App Store 的 URL
 const APP_STORE_URL = "https://apps.apple.com/tw/app/%E7%BE%A4%E7%9B%8A%E8%A1%8C%E5%8B%95%E8%B4%8F%E5%AE%B6/id1469014227";
 
 function handleUniversalLink() {
-    const currentUrl = window.location.href;
+    const currentUrl = decodeURIComponent(window.location.href);
     const appUrl = currentUrl.replace(window.location.protocol + "//", APP_SCHEME);
 
     const start = new Date().getTime();
@@ -18,7 +18,7 @@ function handleUniversalLink() {
 }
 
 function showConfirmDialog() {
-    if (confirm("是否跳轉到 App Store 下載？")) {
+    if (confirm("是否跳轉到 App Store 下載 群益行動贏家？")) {
         redirectToAppStore();
     } else {
         document.getElementById("loading").style.display = "none";
@@ -28,6 +28,14 @@ function showConfirmDialog() {
 
 function redirectToAppStore() {
     window.location.href = APP_STORE_URL;
+}
+
+// 新增：生成編碼後的URL
+function generateEncodedUrl(baseUrl, params) {
+    const encodedParams = Object.entries(params)
+        .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+        .join('&');
+    return `${baseUrl}?${encodedParams}`;
 }
 
 window.onload = handleUniversalLink;
